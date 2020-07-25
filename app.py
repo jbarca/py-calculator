9b190c2fc53fdd6998ea7b8973913b9742e7ccf4
jbarca <jacob.barca1@hotmail.com>
2020-07-25 17:33:04 +1000

    Added index file. Created post requests for each button to functions in app.py.

--- app.py
+++ app.py
@@ -1,12 +1,51 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+first_num = None
+second_num = None
+
+
+# TODO: Add 'operand' variable that denotes which operand will be used in the
+# calculation. When first_num and second_num are both not None and '=' is clicked
+# perform the calculation
 
 @app.route("/")
-def hello():
-    return "hello world"
+def index():
+    return render_template("index.html")
+
+
+@app.route("/handle_number", methods=['POST'])
+def handle_number():
+    request_dict = request.form.to_dict()
+    for key, value in request_dict.items():
+        print(value)
+    return (''), 204
+
+
+@app.route("/add", methods=['POST'])
+def add():
+    print("adding numbers")
+    return (''), 204
+
+
+@app.route("/subtract", methods=['POST'])
+def subtract():
+    print("subtracting numbers")
+    return (''), 204
+
+
+@app.route("/mutliply", methods=['POST'])
+def multiply():
+    print("multiplying numbers")
+    return (''), 204
+
+
+@app.route("/divide", methods=['POST'])
+def divide():
+    print("dividing numbers")
+    return (''), 204
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
